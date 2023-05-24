31436983d9559c470a078fca42575e252ffca255
Matvey6M6 <rolanddiskein666@mail.ru>
2023-05-24 13:48:44 +0300

    micro fix

--- decryption.py
+++ decryption.py
@@ -1,14 +1,14 @@
-from assymetric import assymetric
-from symmetric import symmetric
+from assymetric import Assymetric
+from symmetric import Symmetric
 
 
 def decrypt(encrypted_file, private_key,
                  symmetric_key, decrypted_file, symmetric_key_decrypted, size) :
-    assym_SYM = assymetric(
+    assym_SYM = Assymetric(
         private_k_file=private_key, decrypted_file=symmetric_key_decrypted, ciphed_file=symmetric_key)
 
     assym_SYM.decrypt()
 
-    sym = symmetric(size, symmetric_key_decrypted,
+    sym = Symmetric(size, symmetric_key_decrypted,
                            encrypted_file, decrypted_file)
     sym.decrypt()
--- encryption.py
+++ encryption.py
@@ -1,15 +1,15 @@
-from assymetric import assymetric
-from symmetric import symmetric
+from assymetric import Assymetric
+from symmetric import Symmetric
 
 
 def encrypt(decrypted_file, private_key, symmetric_key, encrypted_file, symmetric_key_decrypted, size) :
 
 
-    assym_SYM = assymetric(
+    assym_SYM = Assymetric(
         private_k_file=private_key, decrypted_file=symmetric_key_decrypted, ciphed_file=symmetric_key)
 
     assym_SYM.decrypt()
 
-    sym = symmetric(size, symmetric_key_decrypted,
+    sym = Symmetric(size, symmetric_key_decrypted,
                            encrypted_file, decrypted_file)
     sym.encrytp()
